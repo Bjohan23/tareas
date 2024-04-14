7748b8287beed0fe42cf14237e1c5fe711e7691c
Bjohan23 <bventurajohanjh@uss.edu.pe>
2024-04-14 12:39:55 -0500

    footer

--- controllers/mostrar.js
+++ controllers/mostrar.js
@@ -2,18 +2,21 @@ const sql = require("../db/db.js");
 
 const mostrar = {
   vistaPrincipal: async (req, res) => {
-    try {
-      const tareas = await sql`SELECT * FROM tarea`;
-      const curso = await sql`SELECT * FROM curso`;
+  try {
+    let tareas = await sql`SELECT * FROM tarea`;
+    const curso = await sql`SELECT * FROM curso`;
 
-      res.render("tarea", { tareas: tareas, curso: curso });
-    } catch (error) {
-      console.error(error);
-      res
-        .status(500)
-        .send("Hubo un error al obtener los datos de la base de datos");
-    }
-  },
+    // Ordenar las tareas por fecha
+    tareas = tareas.sort((a, b) => new Date(a.fecha_limite) - new Date(b.fecha_limite));
+
+    res.render("tarea", { tareas: tareas, curso: curso });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send("Hubo un error al obtener los datos de la base de datos");
+  }
+},
   vistaError: (req, res) => {
     res.status(404).render("404");
   },
